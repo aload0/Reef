319533d5e193ddfef07a149ecf930064e43f3578
Pranav Purwar <purwarpranav80@gmail.com>
2025-09-19 08:13:42 +0530

    Bump to 1.2.1 (8)
    Signed-off-by: Pranav Purwar <purwarpranav80@gmail.com>

--- Reef/build.gradle.kts
+++ Reef/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.pranav.reef"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.2.0"
+        versionCode = 8
+        versionName = "1.2.1"
     }
 
     viewBinding.enable = true
